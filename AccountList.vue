44d1df5ce39bf9f3a92cc4dbe776038e0ceddec6
IsabellaCutillo <99228452+IsabellaCutillo@users.noreply.github.com>
2023-12-06 13:13:10 -0500

    Pages

---
+++ AccountList.vue
@@ -0,0 +1,24 @@
+<script setup>
+import axios from "axios";
+import { Field, Form, ErrorMessage } from 'vee-validate';
+import * as yup from 'yup';
+
+
+async function onSubmit(values) {
+  const response = await axios.get("https://x1r1mjjffe.execute-api.us-east-1.amazonaws.com/customers/" + values.id);
+  console.log(response);
+  alert(JSON.stringify(response, null, 2));
+}
+
+</script>
+
+
+<template>
+  <Form @submit="onSubmit"> 
+
+    <Field name="id" />
+    <ErrorMessage name="id" />
+
+    <button>Get</button>
+  </Form>
+</template>
